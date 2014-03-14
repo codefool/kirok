a2763f6ac83ddc63a602f88abfd8302093abaaad
codefool <ghester@codefool.org>
2014-03-14 15:16:05 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ kirok
 =====
 
 Turn-based Star Trek(tm) space game engine.
+
+This is the main gaming engine. This integrates with the other projects:
+- hexpaper - hex grid geography package
+- drydock - game chit XSD specification and manipulation package
+- helm - text-based front end to Kirok.
---
+++ src/Kirok.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Kirok.cpp
+// Author      : G L Hester
+// Version     :
+// Copyright   : Copyright (C) 2014 All Rights Reserved
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
